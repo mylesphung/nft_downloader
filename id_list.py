cb975ce8af7909f4c9c7cc6d4c650de5fae6c2ed
MP <mylesphung03@gmail.com>
2021-06-04 19:42:32 -0400

    V2.1

--- id_list.py
+++ id_list.py
@@ -4,11 +4,10 @@ path = input("Set path for the downloads: \n")
 os.chdir(path)
 
 name = input("Name for file: \n")
-num = input("Number of NFT's: \n")
+num = int(input("Number of NFT's: \n"))
 
 file = open(name, "a+")
 for i in range(num):
     token = input("Input token id: \n")
-    file.write(token)
-    print("When you are done, type 'Y'")
+    file.write(token+"\n")
 file.close()
