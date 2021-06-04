accecbfce5ff2d4b8eb81cba36cb93c543251f3c
MP <mylesphung03@gmail.com>
2021-06-04 19:02:25 -0400

    V2

--- nft_testing.py
+++ nft_testing.py
@@ -1,11 +1,9 @@
-# token id for testing:
-#29147704762150632971195140556730043302924574719714568449575404612846710947841
-
 #import modules
 import requests
 from PIL import Image
 import os
 
+# get list of token id's
 token_list = []
 print("Type in the list of token ID's, pressing enter after each ID. When you are done, press Y")
 check = False
@@ -15,29 +13,49 @@ while check == False:
         check = True
     else:
         token_list.append(token)
+limit = len(token_list)
 
-#get the data from the API and print the data
+#get and print the data from the API
 url = "https://api.opensea.io/api/v1/assets"
 querystring = {"token_ids": token_list,
-                "order_by":"token_id",
-                "order_direction":"desc",
-                "offset":"0",
-                "limit":"10"}
+                "order_by": "token_id",
+                "order_direction": "desc",
+                "offset": "0",
+                "limit": limit}
 response = requests.get(url=url, params=querystring)
 print(response.text)
 
-# format the data with json and extract the image url
+# format the data with json, and count the number of NFT's
 data = response.json()
-image = data["assets"][0]["image_url"]
-print(image)
+count = len(data["assets"])
+print("NFT Count: " + str(count))
 
-name = data["assets"][0]["name"]
-response = requests.get(image)
-os.chdir("C:\Python/gitHub/nft/datasets/D1")
-file = open(name, "a+b")
-file.write(response.content)
-file.close()
-im = Image.open("nft_test")
-im.show()
+# get the names into a list
+names = []
+for i in range(count):
+    name = data["assets"][(i-1)]["name"]
+    names.append(name)
+print("NFT Names:" + str(names))
 
+# get the images into a list
+images = []
+for i in range(count):
+    image = data["assets"][(i-1)]["image_url"]
+    images.append(image)
+print("NFT Image Links: " + str(images))
 
+# set path to download images to
+path = input("Path for files to be downloaded to (with capitals): \n")
+os.chdir(path)
+print("Path set")
+
+# download the images
+num = 0
+for i in range(count):
+    response = requests.get(images[(i-1)])
+    name = names[(i-1)]
+    file = open(name, "a+b")
+    file.write(response.content)
+    file.close()
+    num+=1
+print("Downloads successful. \n Number of downloads: " + str(num))
